7be02468d9b4db1ad6ee02d952d6bf2f80824596
Ignacio Glinsek <iglinsek@gmail.com>
2017-09-21 23:01:01 -0300

    Added stub css template as Admin page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './theme/App.css';
 import Router from './scenes/Router';
 import registerServiceWorker from './registerServiceWorker';
 
--- src/scenes/Admin/index.js
+++ src/scenes/Admin/index.js
@@ -1,17 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 
 class Admin extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <nav className="navbar navbar-expand-lg bg-primary fixed-top " data-color-on-scroll="400">
+          <div className="container">
+            <div className="navbar-translate">
+              <a className="navbar-brand" href="http://demos.creative-tim.com/now-ui-kit/index.html" data-toggle="tooltip" rel="noopener noreferrer" title="Designed by Invision. Coded by Creative Tim" data-placement="bottom" target="_blank">
+                Now Ui Kit
+            </a>
+              <button className="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-bar bar1"></span>
+                <span className="navbar-toggler-bar bar2"></span>
+                <span className="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
+            <div className="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="../assets/img/blurred-image-1.jpg">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="#pablo">
+                    <p>Link</p>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#pablo">
+                    <p>Link</p>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" rel="noopener noreferrer" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank" >
+                    <i className="fa fa-twitter"></i>
+                    <p className="d-lg-none d-xl-none">Twitter</p>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" rel="noopener noreferrer" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
+                    <i className="fa fa-facebook-square"></i>
+                    <p className="d-lg-none d-xl-none">Facebook</p>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" rel="noopener noreferrer" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
+                    <i className="fa fa-instagram"></i>
+                    <p className="d-lg-none d-xl-none">Instagram</p>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="wrapper">
+          <div className="page-header">
+            <div className="page-header-image" data-parallax="true" style={{ backgroundImage: '' }}>
+            </div>
+          </div>
+          <div className="section"></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <footer className="footer">
+          <div className="container">
+            <nav>
+              <ul>
+                <li>
+                  <a href="https://www.creative-tim.com">
+                    Creative Tim
+                        </a>
+                </li>
+                <li>
+                  <a href="http://presentation.creative-tim.com">
+                    About Us
+                        </a>
+                </li>
+                <li>
+                  <a href="http://blog.creative-tim.com">
+                    Blog
+                        </a>
+                </li>
+                <li>
+                  <a href="https://github.com/creativetimofficial/now-ui-kit/blob/master/LICENSE.md">
+                    MIT License
+                        </a>
+                </li>
+              </ul>
+            </nav>
+            <div className="copyright">
+              &copy;
+                <script>
+                document.write(new Date().getFullYear())
+                </script>, Designed by
+                <a href="http://www.invisionapp.com" target="_blank" rel="noopener noreferrer">Invision</a>. Coded by
+                <a href="https://www.creative-tim.com" target="_blank" rel="noopener noreferrer">Creative Tim</a>.
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
