14afce748d6eaec26cc10b588b4feb949ce1b74d
Ignacio Glinsek <iglinsek@gmail.com>
2017-09-22 16:29:43 -0300

    Added not found scene

---
+++ src/scenes/NotFound/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+import Background from '../../assets/background.jpg';
+
+const NotFound = () => (
+  <div className="page-header filter-color">
+    <div className="page-header-image" style={{ backgroundImage: `url(${Background})` }} />
+    <div className="container" >
+      <div className="col-md-6 content-center">
+        <div className="card card-plain" >
+          <div className="content">
+            <h1 style={{ fontSize: '10em' }}>Oops!</h1>
+            <h6>No podemos encontrar la página que estas buscando</h6>
+          </div>
+          <div className="footer">
+            <Link to="/" className="btn btn-primary btn-round btn-lg">Ir al inicio</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default NotFound;
--- src/scenes/Router/index.js
+++ src/scenes/Router/index.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Admin from '../Admin';
 import Login from '../Login';
+import NotFound from '../NotFound';
 
 const Router = () => (
     <BrowserRouter>
         <div>
             <Route exact path="/" component={Admin} />
             <Route path="/login" component={Login} />
+            <Route component={NotFound} />
         </div>
     </BrowserRouter>
 );
