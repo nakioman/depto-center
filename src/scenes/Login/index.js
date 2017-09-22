dabe0d5466b9fe8c16a3899a9fdcb557cf6c580f
Ignacio Glinsek <iglinsek@gmail.com>
2017-09-22 12:12:22 -0300

    Added login scene

---
+++ src/scenes/Login/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Auth0Lock from 'auth0-lock';
+
+import Background from './assets/background.jpg';
+
+const language = () => {
+    let language = navigator.languages
+        ? navigator.languages[0]
+        : (navigator.language || navigator.userLanguage);
+    if (language.indexOf('-') !== -1) {
+        language = language.split('-')[0];
+    }
+    return language;
+}
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const lock = new Auth0Lock(clientId, domain, {
+    closable: false,
+    language: language(),
+    mustAcceptTerms: true,
+    theme: {
+        logo: '',
+        primaryColor: 'blue',
+    },
+    languageDictionary: {
+        signUpTerms: "Estoy de acuerdo con los <a href='/terms' target='_new'>términos del servicio</a> y la <a href='/privacy' target='_new'>política de privacidad</a>.",
+        title: "Depto center",
+    },
+});
+
+class Login extends Component {
+    componentDidMount() {
+        lock.show();
+    }
+    render() {
+        return (
+            <div style={{
+                background: `url(${Background}) no-repeat center center fixed`,
+                height: '100vh',
+                width: '100vw',
+                backgroundSize: 'cover',
+            }} />
+        );
+    }
+}
+
+export default Login;
--- src/scenes/Router/index.js
+++ src/scenes/Router/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Admin from '../Admin';
+import Login from '../Login';
 
 const Router = () => (
     <BrowserRouter>
-        <Route path="/" component={Admin} />
+        <div>
+            <Route exact path="/" component={Admin} />
+            <Route path="/login" component={Login} />
+        </div>
     </BrowserRouter>
 );
 
